f6dec9f4b0cc25afa05a15dec2ff5e52681c19e1
BugHunter-X <askzenai@gmail.com>
2025-01-03 19:06:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,20 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  const intervalId = useRef(null);
+
+  useEffect(() => {
+    intervalId.current = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId.current);
+  }, []);
+
+  return <div>Count: {count}</div>;
+}
+
+export default MyComponent;
+```
